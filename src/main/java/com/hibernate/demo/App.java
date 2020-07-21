bc39dfd0644b453694b459c1088692856c00955d
junaidansari6311 <junaidansari6311@gmail.com>
2020-07-21 15:03:06 +0530

    ADDED : Functionality to add data to the database in a batch

--- src/main/java/com/hibernate/demo/App.java
+++ src/main/java/com/hibernate/demo/App.java
@@ -1,13 +1,37 @@
 package com.hibernate.demo;
 
-/**
- * Hello world!
- *
- */
+import com.hibernate.demo.model.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        StandardServiceRegistry ssr=new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata meta=new MetadataSources(ssr).getMetadataBuilder().build();
+        SessionFactory factory=meta.getSessionFactoryBuilder().build();
+        Session session=factory.openSession();
+        Transaction tx=session.beginTransaction();
+
+        for ( int i=0; i<250; i++ ) {
+            String fname = "First Name " + i;
+            String lname = "Last Name " + i;
+            Integer salary = i;
+            Employee employee = new Employee(fname, lname, salary);
+            session.save(employee);
+            if( i % 50 == 0 ) {
+                session.flush();
+                session.clear();
+            }
+        }
+
+        tx.commit();
+        session.close();
     }
 }
--- src/main/java/com/hibernate/demo/model/Employee.java
+++ src/main/java/com/hibernate/demo/model/Employee.java
@@ -1,4 +1,4 @@
-package model;
+package com.hibernate.demo.model;
 
 public class Employee {
     private int id;
